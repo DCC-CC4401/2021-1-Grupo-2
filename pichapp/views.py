4c165bfe0ae279299443a56ea9fc6c00d29845cf
Gonzalo <gonzalo.saavedra.m@ug.uchile.cl>
2021-04-22 16:59:32 -0400

    feat [in progress]: Log in
    Error: register not redirecting to login page :-1:

--- pichapp/views.py
+++ pichapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from pichapp.models import User
+from django.contrib.auth import authenticate, login,logout
 
 # Create your views here.
 
@@ -18,7 +19,21 @@ def register_user(request):
         #Crear el nuevo usuario
         user = User.objects.create_user(username=username, password=password, email=email)
 
-     #Redireccionar la página /tareas
-        return HttpResponseRedirect('/tareas')
+     #Redireccionar la página /login
+        return HttpResponseRedirect('/login')
 
     return render(request,"pichapp/register.html")
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request,"pichapp/login.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password=password)
+        if usuario is not None:
+            login(request,user)
+            return HttpResponseRedirect('/home')
+        else:
+            return HttpResponseRedirect('/register')
+

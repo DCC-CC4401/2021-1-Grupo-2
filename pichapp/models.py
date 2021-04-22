bc486794a3e571c0c6304b0b8c94d1b34ec8b09a
Cristóbal Torres <44932278+TinSlim@users.noreply.github.com>
2021-04-22 17:30:22 -0400

    Revert "register & login"

--- pichapp/models.py
+++ pichapp/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-    pronombres = [('La','La'),('El','El'), ('Le','Le'),('Otro','Otro')]
-    #pronombre = models.CharField(max_length=5,choices=pronombres)
-    #apodo = models.CharField(max_length=30)
-    # TODO: user extra atributes

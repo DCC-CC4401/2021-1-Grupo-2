4cfac4eced36640c9b8c2882fca7e2087d2bf1b4
Gonzalo <gonzalo.saavedra.m@ug.uchile.cl>
2021-04-22 15:40:48 -0400

    feat [in progress]: working on register
    Missing html file for reference

--- pichanga/urls.py
+++ pichanga/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register_user, name='register_user'),
+    
 ]
--- pichapp/views.py
+++ pichapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register_user(request):
+    if request.method == 'GET': #Si estamos cargando la página
+        return render(request, "pichapp/templates/pichapp/register.html") #Mostrar el template
+
+    elif request.method == 'POST': #Si estamos recibiendo el form de registro
+     #Tomar los elementos del formulario que vienen en request.POST
+        username = request.POST['username']
+        password = request.POST['password']
+        #apodo = request.POST['apodo']
+        #pronombre = request.POST['pronombre']
+        email = request.POST['email']
+
+        #Crear el nuevo usuario
+        user = User.objects.create_user(username=username, password=password, email=email)
+
+     #Redireccionar la página /tareas
+        return HttpResponseRedirect('/tareas')
+
+   return render(request,"pichapp/templates/pichapp/register.html")

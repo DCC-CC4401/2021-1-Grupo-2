959649583e54140aa76488ee015d0b907251e11e
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2021-04-25 05:48:07 -0400

    feat: add new models to admin site

--- pichapp/admin.py
+++ pichapp/admin.py
@@ -3,4 +3,14 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ActivityCategory)
+class ActivityCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(models.User, UserAdmin)

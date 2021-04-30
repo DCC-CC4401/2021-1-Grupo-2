953ed9d1e8c38f6d70e01b976836f5abad23c4b7
G-Urrea <g.urrea.mansilla@gmail.com>
2021-04-29 21:37:31 -0400

    fixed: urls path

--- pichapp/urls.py
+++ pichapp/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('register/', views.register_user, name='register_user'),
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
-    path('create_room/', views.create_room, name='create_room')
+    path('create_room/', views.create_room, name='create_room'),
     path('rooms/<int:pk>/', views.room_detail, name='room_detail'),
     path('rooms/<int:pk>/participants/join', views.join_room, name='room_join'),
-    path('rooms/<int:pk>/participants/exit', views.exit_room, name='room_exit')
+    path('rooms/<int:pk>/participants/exit', views.exit_room, name='room_exit'),
 ]
